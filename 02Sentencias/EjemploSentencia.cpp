40882585557c76cbc692cafaf9c2139976113538
12Antonio50 <ja.mojica.a@edu.utc.mx>
2022-05-25 13:25:07 -0500

    EjemploFor

--- 02Sentencias/EjemploSentencia.cpp
+++ 02Sentencias/EjemploSentencia.cpp
@@ -6,10 +6,21 @@ int main(){
 	
 	//uso de sentencias de control
 	//do while
-	int i =1;
+	/*int i =1;
 	do{
 		//crear un programa que imprima hasta al 10
 		cout<<i<<endl;
 		i++;
-	}while(i<11);
+	}while(i<11);*/
+	/*int cont = 1;
+	
+	while(cont <=10){
+		cout<<cont<<endl;
+		cont++;
+	}
+	cout<<"se acabo";*/
+	//for
+	for(int i =1; i<=10; i++)
+	
+		cout<<i<<endl;
 }

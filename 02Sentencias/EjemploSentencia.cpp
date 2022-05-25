1692b32bf7e88e5f40e6ff8783268b6ebe10b98e
12Antonio50 <ja.mojica.a@edu.utc.mx>
2022-05-25 13:32:28 -0500

    EjemploSwitch

--- 02Sentencias/EjemploSentencia.cpp
+++ 02Sentencias/EjemploSentencia.cpp
@@ -20,7 +20,32 @@ int main(){
 	}
 	cout<<"se acabo";*/
 	//for
-	for(int i =1; i<=10; i++)
+	/*for(int i =1; i<=10; i++)
 	
-		cout<<i<<endl;
+		cout<<i<<endl;*/
+		
+	int  seleccion;
+	
+	do{
+		cout<<"\n menu 1, 2, 3 otra opcion salir \n";
+		cout<<"\1.- opcion 1 \n";
+		cout<<"\2.- opcion 2 \n";
+		cout<<"\3.- opcion 3 \n";
+		cin>>seleccion;
+		
+		switch(seleccion){
+			case 1:
+				cout<<"Opcion 1";
+				break;
+			case 2:
+				cout<<"Opcion 2";
+				break;
+			case 3:
+				cout<<"Opcion 3";
+				break;
+			default:
+				cout<<"adios";
+				break;
+		}
+	}while((seleccion >0)&&(seleccion <4));
 }
